b61d1c9b988ff245f43d6ae5198c58c02638f365
PRAVEEN <praveenbalajipb14@gmail.com>
2026-03-18 15:31:41 +0530

    Refactor WiFi connection setup code
    Removed redundant variable declarations and improved formatting.

--- wifi-connection.ino
+++ wifi-connection.ino
@@ -1,20 +1,17 @@
 #include <WiFi.h>
+const char* ssid="YOUR_WIFI_NAME";
+const char* password="YOUR_WIFI_PASSWORD";
 
-const char* ssid = "YOUR_WIFI_NAME";
-const char* password = "YOUR_WIFI_PASSWORD";
-
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
-
-  WiFi.begin(ssid, password);
-
+  WiFi.begin(ssid,password);
   Serial.print("Connecting to WiFi");
-
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status()!=WL_CONNECTED) 
+  {
     delay(500);
     Serial.print(".");
   }
-
   Serial.println("\nConnected!");
   Serial.print("ESP32 IP Address: ");
   Serial.println(WiFi.localIP());
